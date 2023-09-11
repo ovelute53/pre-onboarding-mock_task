0adef7268c313194b39ea01d47af4c7014e84a33
ovelute53 <ovelute53@naver.com>
2023-09-11 22:29:51 +0900

    feat: 화면 UI상으로 차트 뿌리기

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,69 @@
-import React = require('react');
+import React, { useState } from 'react';
+import TimeSeriesChart from '@/pages/TimeSeriesChart';
+import mockData from '../public/mock_data.json';
+import styled from 'styled-components';
+import GlobalStyle from '../GlobalStyle';
+
+const dataArray = Object.entries(mockData.response).map(([key, value]) => ({ key, ...value }));
 
 function App() {
-  return <></>;
+  const [selectedID, setSelectedID] = useState<string | null>(null);
+  const filteredData = dataArray.map(d => ({
+    key: d.key,
+    id: d.id,
+    value_area: selectedID === null || selectedID === d.id ? d.value_area : 0,
+    value_bar: selectedID === null || selectedID === d.id ? d.value_bar : 0,
+  }));
+
+  const uniqueIDs = Array.from(new Set(dataArray.map(d => d.id)));
+
+  return (
+    <Container>
+      <GlobalStyle />
+      <FilterContainer>
+        {uniqueIDs.map(id => (
+          <FilterButton key={id} onClick={() => setSelectedID(id)}>
+            {id}
+          </FilterButton>
+        ))}
+      </FilterContainer>
+      <TimeSeriesChart data={filteredData} />
+    </Container>
+  );
 }
 
 export default App;
+
+const Container = styled.div`
+  width: 100%;
+  background-color: #ffffff;
+  padding: 2rem;
+  border-radius: 8px;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+`;
+
+const FilterContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 2rem;
+`;
+
+const FilterButton = styled.button`
+  margin-right: 10px;
+  padding: 8px 12px;
+  border-radius: 4px;
+  border: none;
+  background-color: #007bff;
+  color: #fff;
+  font-weight: bold;
+  cursor: pointer;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+
+  &:active {
+    background-color: #003d80;
+  }
+`;
